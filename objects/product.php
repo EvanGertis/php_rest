592b71f870ab69a229edf741f936e9c281470a10
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-05 14:57:02 -0500

    added read function implementation

--- objects/product.php
+++ objects/product.php
@@ -15,7 +15,23 @@ class Product{
     public $created;
 
     // constructo with $db as database connection.
-    public function _contruct($db){
+    public function __construct($db){
         $this->conn = $db;
+    }
+
+    //read products.
+    function read(){
+
+        //select all query.
+        $query = "SELECT c.name as category_name, p.id, p.name, p.description, FROM
+        ".$this->table_name." p LEFT JOIN categories c ON p.category_id = c.id
+        ORDER BY p.created DESC";
+        
+        //prepare query statement.
+        $stmt = $this->conn->prepare($query);
+
+        //execute query statement.
+        $stmt->execute();
+        return $stmt;
     }
 }
--- product/read.php
+++ product/read.php
@@ -16,3 +16,46 @@ $db = $database->getConnection();
 // initialize the product object.
 $product = new Product($db);
 
+// query products.
+$stmt = $product->read();
+$num = $stmt->rowCount();
+
+// check if more than 0 records are found.
+if($num>0){
+
+    // create products array.
+    $products_arr = array();
+    $products_arr["records"] = array();
+
+    // retrieve our table contents.
+    // fetch() is faster than fetchAll().
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        // extract row.
+        extract($row);
+
+        $product_item= array(
+            "id"=>$id,
+            "name"=>$name,
+            "description"=> html_entity_decode($description),
+            "price"=> $price,
+            "category_id"=> $category_id,
+            "category_name"=> $category_name
+        );
+
+        array_push($products_arr["reocrds"], $product_item);
+    }
+
+    // set response code -200 OK
+    http_response_code(200);
+
+    // show products data in jason format.
+    echo json_encode($products_arr);
+} else {
+    // set the response code to - 404 not found.
+    http_response_code(404);
+
+    // tell the user no products found.
+    echo json_encode(
+        array("message"=> "No products found.")
+    );
+}
