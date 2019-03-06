b2db74efb496429e1cec976a9402b1f154d481af
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-06 10:49:31 -0500

    added create method

--- objects/product.php
+++ objects/product.php
@@ -41,9 +41,10 @@ class Product{
         $query = "INSERT INTO ".$this->table_name."SET name=:name, price=:price, description=:description, category_id=:category_id, created=:created";
 
         // prepare query.
+        $stmt = $this->conn->prepare($query);
 
         // sanitize.
-        $this->name= htmlspecialchars(strip_tags($this->name));
+        $this->name = htmlspecialchars(strip_tags($this->name));
         $this->price = htmlspecialchars(strip_tags($this->price));
         $this->description = htmlspecialchars(strip_tags($this->description));
         $this->category_id = htmlspecialchars(strip_tags($this->category_id));
@@ -55,7 +56,7 @@ class Product{
         $stmt->bindParam(":description", $this->description);
         $stmt->bindParam(":category_id", $this->category_id);
         $stmt->bindParam(":created", $this->created);
-
+        
         //execute query.
         if($stmt->execute()){
             return true;
--- product/create.php
+++ product/create.php
@@ -20,7 +20,6 @@ $product = new Product($db);
 
 // get posted data.
 $data = json_decode(file_get_contents("php://input"));
-
 // make sure data is not empty.
 if(
     !empty($data->name) &&
