b1820933244f57ae7d0f36e3fe8d8eb78297c376
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-07 07:11:07 -0500

    implemented core object and read_paging definition

--- objects/product.php
+++ objects/product.php
@@ -177,4 +177,24 @@ class Product{
 
         return $stmt;
     }
+
+    //read products with pagination.
+    public function readPaging($from_record_num, $records_per_page){
+
+        //select query.
+        $query = "SELECT c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created FROM ".$this->table_name." p LEFT JOIN categories c ON p.category _id = c.id ORDER BY p.created DESC LIMIT ?, ?;";
+        
+        //prepare query statement.
+        $stmt = $this->conn->prepare($query);
+
+        //bind variable valuse.
+        $stmt->bindParam(1, $from_record_num, PDO::PARAM_INT);
+        $stmt->bindParam(2, $records_per_page, PDO::PARAM_INT);
+
+        //execute query.
+        $stmt->execute();
+
+        //return values from database.
+        return $stmt;
+    }
 }
